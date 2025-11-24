e96f7d7de897e6891c8767ad0149f53cdf132d03
marwan agmy <marwanagmy53@gmail.com>
2025-11-24 22:35:25 +0200

    add featured books list view to home view body

--- lib/Features/home/data/presentaion/views/widget/home_view_body.dart
+++ lib/Features/home/data/presentaion/views/widget/home_view_body.dart
@@ -1,34 +1,33 @@
 import 'package:bookly/Features/home/data/presentaion/views/widget/custom_app_bar.dart';
-import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+
+import 'featured_list_view_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(children: [CustomAppBar(), CustomListViewItem()]);
+    return const Column(children: [CustomAppBar(), FeaturedBooksListView()]);
   }
 }
 
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.of(context).size.height * .3,
-      child: AspectRatio(
-        aspectRatio: 2.7 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            image: DecorationImage(
-              fit: BoxFit.fill,
-              image: AssetImage(AssetsData.testImage),
-            ),
-          ),
-        ),
+
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: FeaturedListViewItem(),
+          );
+        },
       ),
     );
   }
