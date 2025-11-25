cfa777a6fb45292858c1339653cbe033dd2f3deb
marwan agmy <marwanagmy53@gmail.com>
2025-11-25 20:35:46 +0200

    create app styles

--- lib/Features/home/data/presentaion/views/widget/home_view_body.dart
+++ lib/Features/home/data/presentaion/views/widget/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly/Features/home/data/presentaion/views/widget/custom_app_bar.dart';
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -17,7 +18,35 @@ class HomeViewBody extends StatelessWidget {
           CustomAppBar(),
           FeaturedBooksListView(),
           SizedBox(height: 50),
-          Text('Best Seller', style: Styles.titleMedium),
+          Text('Best Seller', style: Styles.textStyle18),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.testImage),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/cupertino.dart';
 
 const kPrimaryColor = Color(0xff100B20);
 const kTranstionDuration = Duration(microseconds: 250);
+const kGTSectraFine = 'GT Sectra Fine';
