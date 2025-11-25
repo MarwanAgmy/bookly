9a681aec42e9d9694badb70609dbe9d45c36ee85
marwan agmy <marwanagmy53@gmail.com>
2025-11-25 21:39:50 +0200

    best seller item part2

--- lib/Features/home/data/presentaion/views/widget/home_view_body.dart
+++ lib/Features/home/data/presentaion/views/widget/home_view_body.dart
@@ -11,7 +11,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
+      padding: const EdgeInsets.symmetric(horizontal: 30),
       child: const Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -19,6 +19,7 @@ class HomeViewBody extends StatelessWidget {
           FeaturedBooksListView(),
           SizedBox(height: 50),
           Text('Best Seller', style: Styles.textStyle18),
+          SizedBox(height: 20),
           BestSellerListViewItem(),
         ],
       ),
@@ -46,6 +47,20 @@ class BestSellerListViewItem extends StatelessWidget {
                 ),
               ),
             ),
+          ),
+          SizedBox(width: 30),
+          Column(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .5,
+                child: Text(
+                  'Harry Potter and the Goblet of Fire',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: Styles.textStyle20,
+                ),
+              ),
+            ],
           ),
         ],
       ),
