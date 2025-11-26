db33e46a178aa594737643117f6309185c98ccd7
marwan agmy <marwanagmy53@gmail.com>
2025-11-26 22:01:06 +0200

    add rating to book details

--- lib/Features/home/data/presentaion/views/widget/book_details_view_body.dart
+++ lib/Features/home/data/presentaion/views/widget/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/Features/home/data/presentaion/views/widget/book_rating.dart';
 import 'package:bookly/Features/home/data/presentaion/views/widget/custom_book_details_app_bar.dart';
 import 'package:bookly/Features/home/data/presentaion/views/widget/custom_book_image_item.dart';
 import 'package:bookly/core/utils/styles.dart';
@@ -32,6 +33,8 @@ class BookDetailsViewBody extends StatelessWidget {
                 ),
               ),
             ),
+            const SizedBox(height: 16),
+            const BookRating(mainAxisAlignment: MainAxisAlignment.center),
           ],
         ),
       ),
--- lib/Features/home/data/presentaion/views/widget/book_rating.dart
+++ lib/Features/home/data/presentaion/views/widget/book_rating.dart
@@ -3,22 +3,32 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
-
+  const BookRating({
+    super.key,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+  });
+  final MainAxisAlignment mainAxisAlignment;
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: mainAxisAlignment,
       children: [
-        Icon(FontAwesomeIcons.solidStar, color: Color(0xffFFDD4F)),
+        Icon(FontAwesomeIcons.solidStar, size: 14, color: Color(0xffFFDD4F)),
         const SizedBox(width: 6.3),
         Text(
           '4.8',
-          style: Styles.textStyle16.copyWith(fontWeight: FontWeight.bold),
+          style: Styles.textStyle16.copyWith(
+            fontWeight: FontWeight.bold,
+            color: Color(0xffffffff),
+          ),
         ),
         const SizedBox(width: 5),
-        Text(
-          '(2553)',
-          style: Styles.textStyle14.copyWith(color: Color(0xff707070)),
+        Opacity(
+          opacity: .5,
+          child: Text(
+            '(2553)',
+            style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
+          ),
         ),
       ],
     );
