f27ab0cf6c382dc7f40f092a540ea556fc201dc7
marwan agmy <marwanagmy53@gmail.com>
2025-11-26 23:09:07 +0200

    create books action

--- lib/Features/home/data/presentaion/views/widget/book_details_view_body.dart
+++ lib/Features/home/data/presentaion/views/widget/book_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly/Features/home/data/presentaion/views/widget/book_rating.dart';
+import 'package:bookly/Features/home/data/presentaion/views/widget/books_action.dart';
 import 'package:bookly/Features/home/data/presentaion/views/widget/custom_book_details_app_bar.dart';
 import 'package:bookly/Features/home/data/presentaion/views/widget/custom_book_image_item.dart';
 import 'package:bookly/core/utils/styles.dart';
@@ -35,6 +36,8 @@ class BookDetailsViewBody extends StatelessWidget {
             ),
             const SizedBox(height: 16),
             const BookRating(mainAxisAlignment: MainAxisAlignment.center),
+            const SizedBox(height: 37),
+            const BooksAction(),
           ],
         ),
       ),
